cd064c9cc6509974e66db3861f803f22a0fe1e6b
Slepokurov Valeriy <valeraslp1@gmail.com>
2023-01-20 16:18:30 +0200

    Publication cpp file

--- Library/Publication.cpp
+++ Library/Publication.cpp
@@ -1,1 +1,50 @@
 #include "Publication.h"
+
+Publication::Publication()
+{
+	name = "";
+	author = "";
+	year = 0;
+}
+
+Publication::Publication(string n, string a, int y)
+{
+	name = n;
+	author = a;
+	year = y;
+}
+
+void Publication::setName(string n)
+{
+	name = n;
+}
+
+void Publication::setAuth(string a)
+{
+	author = a;
+}
+
+void Publication::setYear(int y)
+{
+	year = y;
+}
+
+string Publication::getName() const
+{
+	return name;
+}
+
+string Publication::getAuth() const
+{
+	return author;
+}
+
+int Publication::getYear() const
+{
+	return year;
+}
+
+void Publication::show()
+{
+	cout << name << " by " << author << " - " << year << endl;
+}
--- Library/Publication.h
+++ Library/Publication.h
@@ -9,22 +9,19 @@ protected:
 	string name;
 	string author;
 	int year;
-	string desc;
 public:
 	Publication();
-	Publication(string n, string a, int y, string d);
-
-
+	Publication(string n, string a, int y);
 
 	void setName(string n);
 	void setAuth(string a);
 	void setYear(int y);
-	void setDesc(string d);
 
 	string getName() const;
 	string getAuth() const;
 	int getYear() const;
-	string getDesc() const;
+
+	virtual void show();
 };
 
 
